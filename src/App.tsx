efd596a2335ae253e242c31dbed5b6ac14c79649
Mattias Lundmark <lundmark.mattias@gmail.com>
2021-04-22 10:45:33 +0200

    Revert "Fjernet kalkulatorkomponenter da de er flyttet til egen applikasjon"
    This reverts commit 8928f868d6d6b72d85a0412e7e9ac2498c209c25.

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,22 @@ import Permittering from './komponenter/Permittering';
 import './assets/styling/dekorator-override.less';
 import { skrivTilMalingBesokerSide } from './utils/amplitudeUtils';
 import ContextProvider from './komponenter/ContextProvider';
+import Kalkulator from './komponenter/kalkulator/kalkulator';
 import { Brødsmulesti } from './komponenter/Brødsmulesti';
 import { Breadcrumb } from '@navikt/nav-dekoratoren-moduler';
 import './App.less';
 
 export const HOVEDSIDE_PATH = '/arbeidsgiver-permittering';
+export const KALKULATOR_PATH = '/arbeidsgiver-permittering/kalkulator';
+
 export const HOVEDSIDE_BRØDSMULE: Breadcrumb = {
     url: HOVEDSIDE_PATH,
     title: 'Veiviser for permittering',
+    handleInApp: true,
+};
+export const KALKULATOR_BRØDSMULE: Breadcrumb = {
+    url: KALKULATOR_PATH,
+    title: 'Kalkulator',
     handleInApp: true,
 };
 
@@ -29,6 +37,15 @@ const App = () => {
                                     brødsmuler={[HOVEDSIDE_BRØDSMULE]}
                                 />
                                 <Permittering />
+                            </Route>
+                            <Route path={KALKULATOR_PATH} exact={true}>
+                                <Brødsmulesti
+                                    brødsmuler={[
+                                        HOVEDSIDE_BRØDSMULE,
+                                        KALKULATOR_BRØDSMULE,
+                                    ]}
+                                />
+                                <Kalkulator />
                             </Route>
                         </Redirect>
                     </ContextProvider>
